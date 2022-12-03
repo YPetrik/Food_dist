3f0b25d21ea3cacb3377308fbf94f042388b1158
Yuriy <yrii.petrik@gmail.com>
2022-12-03 17:21:56 +0700

    start project

---
+++ js/index.js
@@ -0,0 +1,38 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const tabsContent = document.querySelectorAll(".tabcontent")
+  const tabs = document.querySelectorAll(".tabheader__item")
+  const tabsParent = document.querySelector(".tabheader__items")
+
+  function hideTabContent() {
+    tabsContent.forEach((el) => {
+      el.classList.add("hide")
+      el.classList.remove("show", "fade")
+    })
+
+    tabs.forEach((el) => {
+      el.classList.remove("tabheader__item_active")
+    })
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add("show", "fade")
+    tabsContent[i].classList.remove("hide")
+    tabs[i].classList.add("tabheader__item_active")
+  }
+
+  hideTabContent()
+  showTabContent()
+
+  tabsParent.addEventListener("click", (e) => {
+    e.preventDefault()
+    const target = e.target
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((el, i) => {
+        if (target === el) {
+          hideTabContent()
+          showTabContent(i)
+        }
+      })
+    }
+  })
+})
