f63acb21c51cbdae7ad79964a7a61c2ba9e3aa90
Yuriy <yrii.petrik@gmail.com>
2022-12-05 17:54:56 +0700

    create timer

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
+  // TABS
   const tabsContent = document.querySelectorAll(".tabcontent")
   const tabs = document.querySelectorAll(".tabheader__item")
   const tabsParent = document.querySelector(".tabheader__items")
@@ -35,4 +36,58 @@ window.addEventListener("DOMContentLoaded", () => {
       })
     }
   })
+
+  //TIMER
+
+  const finishSalesDay = "2023-12-24"
+
+  function getAllTime(deadline) {
+    const t = Date.parse(deadline) - Date.parse(new Date())
+    const days = Math.floor(t / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((t / (1000 * 60 * 60)) % 24)
+    const minutes = Math.floor((t / 1000 / 60) % 60)
+    const seconds = Math.floor((t / 1000) % 60)
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    }
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`
+    } else {
+      return num
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector)
+    const days = document.querySelector("#days")
+    const hours = document.querySelector("#hours")
+    const minutes = document.querySelector("#minutes")
+    const seconds = document.querySelector("#seconds")
+
+    timeInterval = setInterval(updateClock, 1000)
+
+    updateClock()
+
+    function updateClock() {
+      const t = getAllTime(endtime)
+
+      days.innerHTML = getZero(t.days)
+      hours.innerHTML = getZero(t.hours)
+      minutes.innerHTML = getZero(t.minutes)
+      seconds.innerHTML = getZero(t.seconds)
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval)
+      }
+    }
+  }
+  setClock(".timer", finishSalesDay)
 })
