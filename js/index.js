9c098199257190929c4792639e4e7bcebc0670c3
Yuriy <yrii.petrik@gmail.com>
2023-01-03 17:33:26 +0700

    timenng open modal window

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
-window.addEventListener("DOMContentLoaded", () => {
-  // TABS
-  const tabsContent = document.querySelectorAll(".tabcontent")
-  const tabs = document.querySelectorAll(".tabheader__item")
-  const tabsParent = document.querySelector(".tabheader__items")
+window.addEventListener("DOMContentLoaded", function () {
+  // Tabs
+
+  let tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    tabsParent = document.querySelector(".tabheader__items")
 
   function hideTabContent() {
-    tabsContent.forEach((el) => {
-      el.classList.add("hide")
-      el.classList.remove("show", "fade")
+    tabsContent.forEach((item) => {
+      item.classList.add("hide")
+      item.classList.remove("show", "fade")
     })
 
-    tabs.forEach((el) => {
-      el.classList.remove("tabheader__item_active")
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active")
     })
   }
 
@@ -24,12 +25,11 @@ window.addEventListener("DOMContentLoaded", () => {
   hideTabContent()
   showTabContent()
 
-  tabsParent.addEventListener("click", (e) => {
-    e.preventDefault()
-    const target = e.target
+  tabsParent.addEventListener("click", function (event) {
+    const target = event.target
     if (target && target.classList.contains("tabheader__item")) {
-      tabs.forEach((el, i) => {
-        if (target === el) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
           hideTabContent()
           showTabContent(i)
         }
@@ -37,16 +37,16 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   })
 
-  //TIMER
+  // Timer
 
-  const finishSalesDay = "2023-12-24"
+  const deadline = "2022-06-11"
 
-  function getAllTime(deadline) {
-    const t = Date.parse(deadline) - Date.parse(new Date())
-    const days = Math.floor(t / (1000 * 60 * 60 * 24))
-    const hours = Math.floor((t / (1000 * 60 * 60)) % 24)
-    const minutes = Math.floor((t / 1000 / 60) % 60)
-    const seconds = Math.floor((t / 1000) % 60)
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      seconds = Math.floor((t / 1000) % 60),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24)
 
     return {
       total: t,
@@ -59,25 +59,24 @@ window.addEventListener("DOMContentLoaded", () => {
 
   function getZero(num) {
     if (num >= 0 && num < 10) {
-      return `0${num}`
+      return "0" + num
     } else {
       return num
     }
   }
 
   function setClock(selector, endtime) {
-    const timer = document.querySelector(selector)
-    const days = document.querySelector("#days")
-    const hours = document.querySelector("#hours")
-    const minutes = document.querySelector("#minutes")
-    const seconds = document.querySelector("#seconds")
-
-    timeInterval = setInterval(updateClock, 1000)
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000)
 
     updateClock()
 
     function updateClock() {
-      const t = getAllTime(endtime)
+      const t = getTimeRemaining(endtime)
 
       days.innerHTML = getZero(t.days)
       hours.innerHTML = getZero(t.hours)
@@ -89,37 +88,56 @@ window.addEventListener("DOMContentLoaded", () => {
       }
     }
   }
-  setClock(".timer", finishSalesDay)
 
-  //modal
+  setClock(".timer", deadline)
 
-  const contactUs = document.querySelectorAll("[data-modal]")
-  const closeModal = document.querySelector("[data-close]")
-  const modalContent = document.querySelector(".modal")
+  // Modal
 
-  contactUs.forEach((el) => {
-    el.addEventListener("click", () => {
-      modalContent.classList.toggle("show")
-      document.body.style.overflow = "hidden"
-    })
+  const modalTrigger = document.querySelectorAll("[data-modal]"),
+    modal = document.querySelector(".modal"),
+    modalCloseBtn = document.querySelector("[data-close]")
+
+  modalTrigger.forEach((btn) => {
+    btn.addEventListener("click", openModal)
   })
 
-  function closeModalFunction() {
-    modalContent.classList.toggle("show")
-    document.body.style.overflow = "hidden"
+  function closeModal() {
+    modal.classList.add("hide")
+    modal.classList.remove("show")
+    document.body.style.overflow = ""
   }
 
-  closeModal.addEventListener("click", closeModalFunction)
+  function openModal() {
+    modal.classList.add("show")
+    modal.classList.remove("hide")
+    document.body.style.overflow = "hidden"
+    clearInterval(modalTimerId)
+  }
 
-  modalContent.addEventListener("click", (e) => {
-    if (e.target === modalContent) {
-      closeModalFunction()
+  modalCloseBtn.addEventListener("click", closeModal)
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      closeModal()
     }
   })
 
   document.addEventListener("keydown", (e) => {
-    if (e.code === "Escape") {
-      closeModalFunction()
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal()
     }
   })
+
+  const modalTimerId = setTimeout(openModal, 3000)
+
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      openModal()
+      window.removeEventListener("scroll", showModalByScroll)
+    }
+  }
+  window.addEventListener("scroll", showModalByScroll)
 })
