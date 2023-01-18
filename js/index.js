2a0dac21bf6c1b0db9ae15415d070301bfb3e121
Yuriy <yrii.petrik@gmail.com>
2023-01-18 18:41:20 +0700

    add new slider

--- js/index.js
+++ js/index.js
@@ -169,45 +169,24 @@ window.addEventListener("DOMContentLoaded", function () {
       }
 
       element.innerHTML = `
-							<img src=${this.src} alt=${this.alt}>
-							<h3 class="menu__item-subtitle">${this.title}</h3>
-							<div class="menu__item-descr">${this.descr}</div>
-							<div class="menu__item-divider"></div>
-							<div class="menu__item-price">
-									<div class="menu__item-cost">Цена:</div>
-									<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-							</div>
-					`
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `
       this.parent.append(element)
     }
   }
 
-  new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    ".menu .container"
-  ).render()
-
-  new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
-    14,
-    ".menu .container"
-  ).render()
-
-  new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    "Меню “Премиум”",
-    "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
-    21,
-    ".menu .container"
-  ).render()
+  getResource("http://localhost:3000/menu").then((data) => {
+    data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuCard(img, altimg, title, descr, price, ".menu .container").render()
+    })
+  })
 
   // Forms
 
@@ -219,35 +198,48 @@ window.addEventListener("DOMContentLoaded", function () {
   }
 
   forms.forEach((item) => {
-    postData(item)
+    bindPostData(item)
   })
 
-  function postData(form) {
+  const postData = async (url, data) => {
+    let res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: data,
+    })
+
+    return await res.json()
+  }
+
+  async function getResource(url) {
+    let res = await fetch(url)
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+    }
+
+    return await res.json()
+  }
+
+  function bindPostData(form) {
     form.addEventListener("submit", (e) => {
       e.preventDefault()
 
       let statusMessage = document.createElement("img")
       statusMessage.src = message.loading
       statusMessage.style.cssText = `
-							display: block;
-							margin: 0 auto;
-					`
+                display: block;
+                margin: 0 auto;
+            `
       form.insertAdjacentElement("afterend", statusMessage)
 
       const formData = new FormData(form)
 
-      const object = {}
-      formData.forEach(function (value, key) {
-        object[key] = value
-      })
+      const json = JSON.stringify(Object.fromEntries(formData.entries()))
 
-      fetch("server.php", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(object),
-      })
+      postData("http://localhost:3000/requests", json)
         .then((data) => {
           console.log(data)
           showThanksModal(message.success)
@@ -271,11 +263,11 @@ window.addEventListener("DOMContentLoaded", function () {
     const thanksModal = document.createElement("div")
     thanksModal.classList.add("modal__dialog")
     thanksModal.innerHTML = `
-					<div class="modal__content">
-							<div class="modal__close" data-close>×</div>
-							<div class="modal__title">${message}</div>
-					</div>
-			`
+            <div class="modal__content">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `
     document.querySelector(".modal").append(thanksModal)
     setTimeout(() => {
       thanksModal.remove()
@@ -285,7 +277,51 @@ window.addEventListener("DOMContentLoaded", function () {
     }, 4000)
   }
 
-  fetch("/db.json")
-    .then((respons) => respons.json())
-    .then((res) => console.log(res))
+  // Slider
+
+  let slideIndex = 1
+  const slides = document.querySelectorAll(".offer__slide"),
+    prev = document.querySelector(".offer__slider-prev"),
+    next = document.querySelector(".offer__slider-next"),
+    total = document.querySelector("#total"),
+    current = document.querySelector("#current")
+
+  showSlides(slideIndex)
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`
+  } else {
+    total.textContent = slides.length
+  }
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+      slideIndex = slides.length
+    }
+
+    slides.forEach((item) => (item.style.display = "none"))
+
+    slides[slideIndex - 1].style.display = "block" // Как ваша самостоятельная работа - переписать на использование классов show/hide
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`
+    } else {
+      current.textContent = slideIndex
+    }
+  }
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n))
+  }
+
+  prev.addEventListener("click", function () {
+    plusSlides(-1)
+  })
+
+  next.addEventListener("click", function () {
+    plusSlides(1)
+  })
 })
